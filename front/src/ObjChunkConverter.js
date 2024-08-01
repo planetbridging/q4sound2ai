c9b72e8f45f343ea4a864442683c4d84b5e75ac1
planetbridging <47130739+planetbridging@users.noreply.github.com>
2024-08-02 07:20:40 +1000

    created chunk convert to spectrogram
    need to make it append to array not replace it

--- front/src/ObjChunkConverter.js
+++ front/src/ObjChunkConverter.js
@@ -1,9 +1,118 @@
 import React, { Component } from "react";
-import { Box, VStack, Text, Flex, Spacer } from "@chakra-ui/react";
+import { Box, VStack, Text, Flex, Spacer, Button } from "@chakra-ui/react";
+import WaveSurfer from "wavesurfer.js";
+import TimelinePlugin from "wavesurfer.js/dist/plugins/timeline";
+import SpectrogramPlugin from "wavesurfer.js/dist/plugins/spectrogram";
+
+import { convertToWavChunk } from "./audioUtils";
 
 class ObjChunkConverter extends Component {
+  constructor(props) {
+    super(props);
+    this.waveSurferRef = React.createRef();
+    this.timelineRef = React.createRef();
+    this.spectrogramRef = React.createRef();
+    this.state = {
+      chunkUrl: null,
+      chunkDuration: 0,
+      isPlaying: false,
+      labeledData: [],
+    };
+  }
+
+  handleGenerateSpectrogram = async (chunkData) => {
+    const { uniqPassFile, uniqPassIn, lstCats } = chunkData;
+    const start = uniqPassIn.start;
+    const end = uniqPassIn.end;
+
+    try {
+      const chunkUrl = await convertToWavChunk(uniqPassFile, start, end, null);
+      this.setState({ chunkUrl }, () => {
+        this.initWaveSurfer(chunkUrl, lstCats);
+      });
+    } catch (error) {
+      console.error("Error generating spectrogram:", error);
+    }
+  };
+
+  initWaveSurfer = (url, labels) => {
+    if (this.waveSurferInstance) {
+      this.waveSurferInstance.destroy();
+    }
+
+    this.waveSurferInstance = WaveSurfer.create({
+      container: this.waveSurferRef.current,
+      waveColor: "violet",
+      progressColor: "purple",
+      responsive: true,
+      height: 100,
+      barWidth: 2,
+      cursorWidth: 1,
+      backend: "WebAudio",
+      plugins: [
+        TimelinePlugin.create({
+          container: this.timelineRef.current,
+        }),
+        SpectrogramPlugin.create({
+          container: this.spectrogramRef.current,
+          labels: true,
+        }),
+      ],
+    });
+
+    this.waveSurferInstance.load(url);
+    this.waveSurferInstance.on("ready", () => {
+      const duration = this.waveSurferInstance.getDuration();
+      this.setState({ chunkDuration: duration });
+
+      // Extract spectrogram data
+      this.extractSpectrogramData(labels);
+    });
+    this.waveSurferInstance.on("error", (error) => {
+      console.error("Error loading audio data:", error);
+    });
+    this.waveSurferInstance.on("play", () =>
+      this.setState({ isPlaying: true })
+    );
+    this.waveSurferInstance.on("pause", () =>
+      this.setState({ isPlaying: false })
+    );
+  };
+
+  extractSpectrogramData = (labels) => {
+    // Extract spectrogram data from the spectrogram plugin
+    const canvas = this.spectrogramRef.current.querySelector("canvas");
+    const context = canvas.getContext("2d");
+    const pixelData = context.getImageData(
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    ).data;
+
+    const spectrogramData = [];
+    for (let i = 0; i < pixelData.length; i += 4) {
+      const avg = (pixelData[i] + pixelData[i + 1] + pixelData[i + 2]) / 3;
+      spectrogramData.push(avg / 255.0); // Normalize to [0, 1]
+    }
+
+    this.setState((prevState) => ({
+      labeledData: [
+        ...prevState.labeledData,
+        { spectrogram: spectrogramData, labels: labels },
+      ],
+    }));
+  };
+
+  handlePlayPause = () => {
+    if (this.waveSurferInstance) {
+      this.waveSurferInstance.playPause();
+    }
+  };
+
   render() {
     const { mapChunkWithLabel } = this.props;
+    const { chunkUrl, chunkDuration, isPlaying, labeledData } = this.state;
 
     return (
       <Box>
@@ -29,12 +138,34 @@ class ObjChunkConverter extends Component {
                     <Text>Category: {chunkData.lstCats[0]}</Text>
                     <Text>Subcategory: {chunkData.lstCats[1]}</Text>
                     <Text>Sub-subcategory: {chunkData.lstCats[2]}</Text>
+                    <Button
+                      onClick={() => this.handleGenerateSpectrogram(chunkData)}
+                    >
+                      Generate Spectrogram
+                    </Button>
                   </VStack>
                 </Flex>
               </Box>
             )
           )}
         </VStack>
+        {chunkUrl && (
+          <Box mt={4}>
+            <Text mb={2}>
+              Spectrogram (Duration: {chunkDuration.toFixed(2)}s):
+            </Text>
+            <div ref={this.waveSurferRef}></div>
+            <div ref={this.timelineRef}></div>
+            <div ref={this.spectrogramRef}></div>
+            <Button onClick={this.handlePlayPause}>
+              {isPlaying ? "Pause" : "Play"}
+            </Button>
+          </Box>
+        )}
+        <Box mt={4}>
+          <Text>Labeled Data:</Text>
+          <pre>{JSON.stringify(labeledData, null, 2)}</pre>
+        </Box>
       </Box>
     );
   }
--- front/src/audioUtils.js
+++ front/src/audioUtils.js
@@ -35,4 +35,48 @@ const convertToWav = async (file, onProgress) => {
   }
 };
 
-export { convertToWav };
+const convertToWavChunk = async (file, start, end, onProgress) => {
+  if (!ffmpeg.loaded) {
+    await ffmpeg.load();
+  }
+
+  const fileType = file.name.split(".").pop().toLowerCase();
+  const inputFileName = `input.${fileType}`;
+  const outputFileName = "output_chunk.wav";
+
+  try {
+    await ffmpeg.writeFile(inputFileName, await fetchFile(file));
+    await ffmpeg.exec([
+      "-i",
+      inputFileName,
+      "-ss",
+      `${start}`,
+      "-to",
+      `${end}`,
+      "-ar",
+      "44100",
+      "-ac",
+      "2",
+      "-c:a",
+      "pcm_s16le",
+      outputFileName,
+    ]);
+
+    const data = await ffmpeg.readFile(outputFileName);
+    const blob = new Blob([data.buffer], { type: "audio/wav" });
+    const url = URL.createObjectURL(blob);
+
+    if (onProgress) {
+      onProgress(100);
+    }
+
+    console.log("Generated WAV URL:", url); // Debug: Log the generated URL
+
+    return url;
+  } catch (error) {
+    console.error("Error in convertToWavChunk:", error);
+    throw error;
+  }
+};
+
+export { convertToWav, convertToWavChunk };
